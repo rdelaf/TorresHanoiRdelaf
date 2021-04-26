6f6876a1a4d2e50d32eda259377a58337abc0328
Raúl de la Fe Robles <rdelaf@upv.edu.es>
2021-04-26 15:28:26 +0200

    Finish

---
+++ torres-de-hanoi/Torres_de_Hanoi/Pila.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Pila
+    {
+        public int Size { get; set; }
+        public int Top { get; set; }
+        public List<Disco> Elementos { get; set; }
+
+        public Pila()
+        {
+            Size = 0;
+            Top = 0;
+            Elementos = new List<Disco>();
+        }
+
+        //Añade un nuevo elemento a la pila
+        public void push(Disco d)
+        {
+            Elementos.Add(d);
+            Top = d.Valor;
+            Size++;
+        }
+
+        //Devuelve el último elemento de la pila y lo elimmina
+        public Disco pop()
+        {
+            if (Size > 0)
+            {
+                Disco discoRetirado = new Disco();
+                discoRetirado.Valor = Elementos[Size - 1].Valor;
+                Elementos.RemoveAt(Elementos.Count() - 1);
+                Size--;
+                if (Size > 0) Top = Elementos[Size - 1].Valor;
+                else Top = 0;
+                return discoRetirado;
+            }
+            return null;
+        }                
+
+        //Devuelve true si la pila está vacía y false si no
+        public bool isEmpty()
+        {
+            if (Size <= 0) return true;
+            else return false;
+        }
+    }
+}
---
+++ torres-de-hanoi/Torres_de_Hanoi/bin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Pila INI = new Pila();
+          Pila AUX = new Pila();
+          Pila FIN = new Pila();
+
+          Console.WriteLine("¿Con cuantos discos deseas jugar?");
+          int numeroDiscos = Convert.ToInt32(Console.ReadLine());
+
+          for(int i = 0; i<numeroDiscos; i++)
+          {
+              Disco d = new Disco();
+              d.Valor = i+1;
+              INI.push(d);
+              INI.Top = d.Valor;
+          }
+
+          Console.WriteLine("Inicio.");
+          Hanoi hanoi = new Hanoi();
+          hanoi.iterativo(numeroDiscos, INI, AUX, FIN);
+          Console.WriteLine("Inicio Algoritmo.");
+          hanoi.recursivo(numeroDiscos, INI, AUX, FIN);
+
+          Console.WriteLine("Presione cualquier tecla para salir.");
+          Console.ReadKey();
+        }
+    }
+}
